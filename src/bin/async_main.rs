1f7812949bbecc471a313f4fc3ddb22e21fe77bc
Luiz Mendonca <luquemendonca@gmail.com>
2025-03-22 20:32:37 +0100

    spi master slave workign 2 boards

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -51,11 +51,10 @@ use esp_hal::{
     spi,
 };
 use esp_println::println;
-static mut MASTER_SEND: [u8; 32000] = [0; 32000];
-static mut MASTER_RECEIVE: [u8; 32000] = [0; 32000];
+
 use core::cell::RefCell;
 
-static SIGNAL: Watch<CriticalSectionRawMutex, bool, 4> = Watch::new();
+static SIGNAL: Watch<CriticalSectionRawMutex, bool, 6> = Watch::new();
 static BUTTON: critical_section::Mutex<RefCell<Option<Input>>> =
     critical_section::Mutex::new(RefCell::new(None));
 #[esp_hal_embassy::main]
@@ -69,15 +68,10 @@ async fn main(spawner: Spawner) {
     esp_hal_embassy::init(timg0.timer0);
 
     let mut io = Io::new(peripherals.IO_MUX);
+    esp_alloc::heap_allocator!(size: 72 * 1024);
     io.set_interrupt_handler(handler);
 
-    let mut master_sclk = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
-    let master_miso = Input::new(
-        peripherals.GPIO5,
-        InputConfig::default().with_pull(Pull::None),
-    );
-    let mut master_mosi = Output::new(peripherals.GPIO6, Level::Low, OutputConfig::default());
-    let mut master_cs = Output::new(peripherals.GPIO7, Level::High, OutputConfig::default());
+
 
     let slave_sclk = peripherals.GPIO10;
     let slave_miso = peripherals.GPIO11;
@@ -108,14 +102,12 @@ async fn main(spawner: Spawner) {
     let delay = Delay::new();
 
     // DMA buffer require a static life-time
-    let master_send = unsafe { &mut MASTER_SEND };
-    let master_receive = unsafe { &mut MASTER_RECEIVE };
+
     let mut slave_send = tx_buffer;
     let mut slave_receive = rx_buffer;
     let mut i = 0;
 
-    // Initialize master_send with a simple pattern
-    master_send.fill(0xAA); // Fill with pattern 10101010
+
 
     let config = InputConfig::default().with_pull(Pull::Up);
     let mut cs_interrupt: Input<'_> = Input::new(slave_cs, config);
@@ -131,109 +123,47 @@ async fn main(spawner: Spawner) {
     let mut recv = SIGNAL.receiver().unwrap();
 
     loop {
-        // Update a few key positions to track iterations
-        master_send[0] = i; // First byte shows iteration count
-        master_send[1] = 0xBB; // Fixed marker
-        master_send[2] = 0xCC; // Fixed marker
-        master_send[master_send.len() - 1] = i; // Last byte also shows iteration count
 
-        i = i.wrapping_add(1); // Increment counter, wrapping at 255
         slave_receive.fill(0xff);
 
-        println!("Do `read`");
-        slave_receive.fill(0xff);
-
+        println!("SPI READ");
         let transfer = spi.read(&mut slave_receive).unwrap();
 
-        bitbang_master(
-            master_send,
-            master_receive,
-            &mut master_cs,
-            &mut master_mosi,
-            &mut master_sclk,
-            &master_miso,
-        )
-        .await;
-
-        println!("pipoca");
+        // Wait for the transfer to complete
+        println!("CS WAIT in MAIN");
         recv.changed().await;
         drop(transfer);
+        println!("SPI GOT data in MAIN");
 
-        println!("doce");
-
-        // println!(
-        //     "slave got {:x?} .. {:x?}",
-        //     &slave_receive[..10],
-        //     &slave_receive[slave_receive.len() - 10..],
-        // );
-
-        // Add a delay between iterations
-        Timer::after(Duration::from_millis(1000)).await;
-
-        println!();
-    }
-}
-
-async fn bitbang_master(
-    master_send: &[u8],
-    master_receive: &mut [u8],
-    master_cs: &mut Output<'_>,
-    master_mosi: &mut Output<'_>,
-    master_sclk: &mut Output<'_>,
-    master_miso: &Input<'_>,
-) {
-    // Bit-bang out the contents of master_send and read into master_receive
-    // as quickly as manageable. MSB first. Mode 0, so sampled on the rising
-    // edge and set on the falling edge.
-    println!("bitbang_master writing to spi");
-    master_cs.set_low();
-    for (j, v) in master_send.iter().enumerate() {
-        let mut b = *v;
-        let mut rb = 0u8;
-        for _ in 0..8 {
-            if b & 128 != 0 {
-                master_mosi.set_high();
-            } else {
-                master_mosi.set_low();
-            }
-            master_sclk.set_low();
-            b <<= 1;
-            rb <<= 1;
-            // NB: adding about 24 NOPs here makes the clock's duty cycle
-            // run at about 50% ... but we don't strictly need the delay,
-            // either.
-            master_sclk.set_high();
-            if master_miso.is_high() {
-                rb |= 1;
-            }
-        }
-        master_receive[j] = rb;
-    }
-    master_sclk.set_low();
-
-    // Add a small delay before raising CS to ensure data transmission is complete
-
-    master_cs.set_high();
-    master_sclk.set_low();
-}
-
-#[embassy_executor::task]
-async fn get_slave_dma_buffer(slave_receive_ptr: *const [u8]) {
-    let mut recv = SIGNAL.receiver().unwrap();
-    loop {
-        println!("MORANGO");
-        recv.changed().await;
-        println!("ABACATE");
-
-        // UNSafely dereference the pointer to access the buffer (this is just an example showing you can acces the data even from a different task)
-        // In a real application, you should ensure that the pointer is valid and properly synchronized.
-        let slave_receive = unsafe { &*slave_receive_ptr };
 
         println!(
             "slave got {:x?} .. {:x?}",
             &slave_receive[..10],
             &slave_receive[slave_receive.len() - 10..],
         );
+
+        // Add a delay between iterations
+        //Timer::after(Duration::from_millis(1000)).await;
+
+
+    }
+}
+
+
+#[embassy_executor::task]
+async fn get_slave_dma_buffer(slave_receive_ptr: *const [u8]) {
+    let mut recv = SIGNAL.receiver().unwrap();
+    let slave_receive = unsafe { &*slave_receive_ptr };
+    loop {
+        println!("Wait CS in TASK ");
+        recv.changed().await;
+        println!("GOT CS in TASK ");
+
+        // UNSafely dereference the pointer to access the buffer (this is just an example showing you can acces the data even from a different task)
+        // In a real application, you should ensure that the pointer is valid and properly synchronized.
+        
+
+        
     }
 }
 
@@ -258,7 +188,7 @@ fn handler() {
             .unwrap()
             .is_interrupt_set()
     }) {
-        esp_println::println!("Button was the source of the interrupt");
+        esp_println::println!("Button was the source of the INTERRUPT");
         let sender = SIGNAL.sender();
         sender.send(true);
     } else {
